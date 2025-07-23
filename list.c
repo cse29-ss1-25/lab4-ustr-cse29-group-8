31d0da6154ac52d020a9e0004eb0bf85b656c0a9
A. Uma Mahesh <umamahesh@As-MacBook-Air.attlocal.net>
2025-07-23 12:42:55 -0700

    Vamsi and Andrew finished implementation

--- list.c
+++ list.c
@@ -8,7 +8,8 @@
 /*
 Returns an empty list of initial_capacity
 */
-List new_list(int32_t initial_capacity) {
+List new_list(int32_t initial_capacity)
+{
     List list;
     list.size = 0;
     list.capacity = initial_capacity;
@@ -19,23 +20,52 @@ List new_list(int32_t initial_capacity) {
 /*
 Initializes an list of length size with elements from array
 */
-List new_list_from_array(struct UStr* array, int32_t size) {
+List new_list_from_array(struct UStr *array, int32_t size)
+{
     List list = new_list(size);
-    for (int32_t i = 0; i < size; i++) {
+    for (int32_t i = 0; i < size; i++)
+    {
         list.data[i] = array[i];
     }
     list.size = size;
     return list;
 }
 
-
 /*
-Given a list of strings and a separator string, returns a single string 
+Given a list of strings and a separator string, returns a single string
 containing all the strings in list joined by the separator.
 */
-UStr join(List* list, UStr separator) {
+UStr join(List *list, UStr separator)
+{
     // TODO: implement this
-
+    if (list->size == 0)
+    {
+        return new_ustr("");
+    }
+    int total_bytes = 0;
+    for (int i = 0; i < list->size; i++)
+    {
+        total_bytes += list->data[i].bytes;
+    }
+    total_bytes += separator.bytes * (list->size - 1);
+    char *buffer = malloc(total_bytes + 1);
+    if (!buffer)
+        return new_ustr("");
+    int offset = 0;
+    for (int i = 0; i < list->size; i++)
+    {
+        memcpy(buffer + offset, list->data[i].contents, list->data[i].bytes);
+        offset += list->data[i].bytes;
+        if (i < list->size - 1)
+        {
+            memcpy(buffer + offset, separator.contents, separator.bytes);
+            offset += separator.bytes;
+        }
+    }
+    buffer[offset] = '\0';
+    UStr result = new_ustr(buffer);
+    free(buffer);
+    return result;
 }
 
 /*
@@ -44,9 +74,33 @@ Expands the list's capacity if necessary (double the capacity, or set to 1 if 0)
 
 Returns 1 on success, 0 if the index is invalid (out of bounds).
 */
-int8_t insert(List* list, UStr s, int32_t index) {
+int8_t insert(List *list, UStr s, int32_t index)
+{
     // TODO: implement this
-
+    if (index < 0 || index > list->size)
+    {
+        return 0;
+    }
+    // Expand capacity
+    if (list->size >= list->capacity)
+    {
+        int32_t new_capacity = list->capacity == 0 ? 1 : list->capacity * 2;
+        UStr *new_data = realloc(list->data, new_capacity * sizeof(UStr));
+        if (new_data == NULL)
+        {
+            return 0;
+        }
+        list->data = new_data;
+        list->capacity = new_capacity;
+    }
+    // Shift right
+    for (int32_t i = list->size; i > index; i--)
+    {
+        list->data[i] = list->data[i - 1];
+    }
+    list->data[index] = s;
+    list->size++;
+    return 1;
 }
 
 /*
@@ -54,9 +108,27 @@ Removes the element at the given index and shifts all subsequent elements left.
 
 Returns 1 on success, 0 if the index is invalid (out of bounds).
 */
-int8_t listRemoveAt(List* list, int32_t index) {
+int8_t listRemoveAt(List *list, int32_t index)
+{
     // TODO: implement this
+    if (index < 0 || index >= list->size)
+    {
+        return 0;
+    }
 
+    free_ustr(list->data[index]);
+
+    // Shift left
+    for (int i = index; i < list->size - 1; i++)
+    {
+        list->data[i] = list->data[i + 1];
+    }
+
+    list->data[list->size - 1].contents = NULL;
+
+    list->size--;
+
+    return 1;
 }
 
 /*
@@ -64,15 +136,74 @@ Splits the given string s into substrings separated by the given delimiter strin
 
 Returns a List of String objects, each containing a segment between delimiters.
 
-If the delimiter is the empty string, return a list containing the original 
+If the delimiter is the empty string, return a list containing the original
 string as a single element.
 
-If the input string ends with the delimiter, include an empty string at the 
+If the input string ends with the delimiter, include an empty string at the
 end of the result.
 
 Note that the delimiter could be of a length of more than 1 character
 */
-List split(UStr s, UStr separator) {
+List split(UStr s, UStr separator)
+{
     // TODO: implement this
+    if (separator.bytes == 0)
+    {
 
+        UStr *single = malloc(sizeof(UStr));
+        *single = new_ustr(s.contents);
+        return new_list_from_array(single, 1);
+    }
+
+    UStr *result_array = malloc(sizeof(UStr) * (s.codepoints + 1));
+    int32_t count = 0;
+    int32_t start = 0;
+    int32_t i = 0;
+
+    while (i <= s.bytes - separator.bytes)
+    {
+        int match = 1;
+        for (int j = 0; j < separator.bytes; j++)
+        {
+            if (s.contents[i + j] != separator.contents[j])
+            {
+                match = 0;
+                break;
+            }
+        }
+
+        if (match)
+        {
+
+            int segment_len = i - start;
+            char *buffer = malloc(segment_len + 1);
+            int write = 0;
+            for (int k = start; k < i; k++)
+            {
+                buffer[write++] = s.contents[k];
+            }
+            buffer[write] = '\0';
+            result_array[count++] = new_ustr(buffer);
+            free(buffer);
+
+            i += separator.bytes;
+            start = i;
+        }
+        else
+        {
+            i++;
+        }
+    }
+
+    char *buffer = malloc(s.bytes - start + 1);
+    int write = 0;
+    for (int k = start; k < s.bytes; k++)
+    {
+        buffer[write++] = s.contents[k];
+    }
+    buffer[write] = '\0';
+    result_array[count++] = new_ustr(buffer);
+    free(buffer);
+
+    return new_list_from_array(result_array, count);
 }
--- ustr.c
+++ ustr.c
@@ -8,78 +8,200 @@
 /*
 Initializes a new UStr with contents
 */
-UStr new_ustr(char* contents) {
+UStr new_ustr(char *contents)
+{
 	int32_t bytes = strlen(contents);
 	int32_t codepoints = utf8_strlen(contents);
 	uint8_t all_ascii = bytes == codepoints;
-	char* contents_copy = malloc(bytes + 1);
+	char *contents_copy = malloc(bytes + 1);
 	strcpy(contents_copy, contents);
 	UStr s = {
 		codepoints,
 		bytes,
 		all_ascii,
-		contents_copy
-	};
+		contents_copy};
 	return s;
 }
 
 /*
 Returns length of string
 */
-int32_t len(UStr s) {
+int32_t len(UStr s)
+{
 	return s.codepoints;
 }
 
 /*
-Returns a substring of str, starting at index start (inclusive) 
+Returns a substring of str, starting at index start (inclusive)
 and ending at index end (exclusive).
 
 Returns an empty string on invalid range.
 */
-UStr substring(UStr s, int32_t start, int32_t end) {
+UStr substring(UStr s, int32_t start, int32_t end)
+{
 	// TODO: implement this
-
+	if (start < 0 || end > s.codepoints || start >= end)
+	{
+		return new_ustr("");
+	}
+	int32_t byte_start = bi_of_cpi(s.contents, start);
+	int32_t byte_end = bi_of_cpi(s.contents, end);
+	if (byte_start < 0 || byte_end < 0 || byte_start >= byte_end)
+	{
+		return new_ustr("");
+	}
+	int32_t sub_len = byte_end - byte_start;
+	char *buffer = malloc(sub_len + 1);
+	if (buffer == NULL)
+	{
+		return new_ustr("");
+	}
+	memcpy(buffer, s.contents + byte_start, sub_len);
+	buffer[sub_len] = '\0';
+	UStr result = new_ustr(buffer);
+	free(buffer);
+	return result;
 }
 
 /*
-Given 2 strings s1 and s2, returns a string that is the result of 
-concatenating s1 and s2. 
+Given 2 strings s1 and s2, returns a string that is the result of
+concatenating s1 and s2.
 */
-UStr concat(UStr s1, UStr s2) {
+UStr concat(UStr s1, UStr s2)
+{
 	// TODO: implement this
+	int total_bytes = s1.bytes + s2.bytes;
+	char *new_contents = malloc(total_bytes + 1);
+	if (new_contents == NULL)
+	{
 
+		UStr empty = {0, 0, 1, NULL};
+		return empty;
+	}
+
+	int i = 0;
+	for (int j = 0; j < s1.bytes; j++)
+	{
+		new_contents[i] = s1.contents[j];
+		i++;
+	}
+	for (int j = 0; j < s2.bytes; j++)
+	{
+		new_contents[i] = s2.contents[j];
+		i++;
+	}
+	new_contents[i] = '\0';
+
+	UStr result = {
+		.codepoints = s1.codepoints + s2.codepoints,
+		.bytes = total_bytes,
+		.contents = new_contents,
+		.is_ascii = s1.is_ascii && s2.is_ascii};
+	return result;
 }
 
 /*
-Given a string s and an index, return a string with the character at index 
-removed from the original string. 
+Given a string s and an index, return a string with the character at index
+removed from the original string.
 
 Returns the original string if index is out of bounds.
 */
-UStr removeAt(UStr s, int32_t index) {
+UStr removeAt(UStr s, int32_t index)
+{
 	// TODO: implement this
+	if (index < 0 || index >= s.codepoints)
+	{
 
+		return new_ustr(s.contents);
+	}
+
+	char *new_contents = malloc(s.bytes + 1);
+	if (new_contents == NULL)
+	{
+		return new_ustr("");
+	}
+
+	int32_t cpi = 0;
+	int32_t read = 0;
+	int32_t write = 0;
+
+	while (s.contents[read] != '\0')
+	{
+		if (cpi == index)
+		{
+
+			int skip_bytes = utf8_codepoint_size(s.contents[read]);
+			read += skip_bytes;
+			cpi++;
+			continue;
+		}
+
+		int cp_size = utf8_codepoint_size(s.contents[read]);
+		for (int i = 0; i < cp_size; i++)
+		{
+			new_contents[write++] = s.contents[read++];
+		}
+		cpi++;
+	}
+
+	new_contents[write] = '\0';
+
+	UStr result = new_ustr(new_contents);
+	free(new_contents);
+	return result;
 }
 
 /*
-Given a string s, return s reversed. 
+Given a string s, return s reversed.
 
 Example: reverse("apples🍎 and bananas🍌") = "🍌sananab dna 🍎selppa")
 */
-UStr reverse(UStr s) {
+UStr reverse(UStr s)
+{
 	// TODO: implement this
+	const char *input = s.contents;
+	int32_t len = s.bytes;
+	char *reversed = malloc(len + 1);
+	if (reversed == NULL)
+	{
+		return new_ustr("");
+	}
 
+	int32_t read_index = len - 1;
+	int32_t write_index = 0;
+	while (read_index >= 0)
+	{
+
+		while (read_index > 0 && is_continuation_byte((unsigned char)input[read_index]))
+		{
+			read_index--;
+		}
+		int8_t cp_size = utf8_codepoint_size(input[read_index]);
+		if (cp_size <= 0)
+		{
+			free(reversed);
+			return new_ustr("");
+		}
+		memcpy(reversed + write_index, input + read_index, cp_size);
+		write_index += cp_size;
+		read_index--;
+	}
+	reversed[write_index] = '\0';
+	UStr result = new_ustr(reversed);
+	free(reversed);
+	return result;
 }
 
-
-void print_ustr(UStr s) {
+void print_ustr(UStr s)
+{
 	printf("%s [codepoints: %d | bytes: %d]", s.contents, s.codepoints, s.bytes);
 }
 
-void free_ustr(UStr s) {
-	if (s.contents != NULL) {
+void free_ustr(UStr s)
+{
+	if (s.contents != NULL)
+	{
 		free(s.contents);
 		s.contents = NULL;
 	}
 }
-
